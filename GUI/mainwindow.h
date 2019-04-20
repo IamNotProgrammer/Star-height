8de25c15c2c6a44de416d9513a92bf1f4a4f59ff
IamNotProgrammer <49314448+IamNotProgrammer@users.noreply.github.com>
2019-04-20 21:40:23 +0200

    Add files via upload

--- GUI/mainwindow.h
+++ GUI/mainwindow.h
@@ -17,6 +17,18 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    #define PI 3.1415926536 // it will be usefull to change into radians
+// every comment to variable is in order i.e. int a, b ; // explained a, explained b
+    int year = 2019 ; // current year
+    int mon2 = 5 ; // current month
+    int day2 = 1 ; // current day
+
+    int H, M, L_H, L_M ; // hour and minute to clock: local hour, local minute, LST hour, LST minute
+
+    float d, a, l, phi ; // coordinates: declination, rectastence, longitude, latitude
+    float UT, LST, LMST, t, CT ; // time: Greenwich time, local siderial time, local time, hour angle, changing time (in function)
+
+
 };
 
 #endif // MAINWINDOW_H
