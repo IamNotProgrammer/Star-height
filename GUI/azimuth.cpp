3405fc825b53db291aa25da134001f3eac814636
IamNotProgrammer <49314448+IamNotProgrammer@users.noreply.github.com>
2020-02-29 22:59:59 +0100

    Add files via upload

--- GUI/azimuth.cpp
+++ GUI/azimuth.cpp
@@ -1,17 +1,11 @@
 #include "azimuth.h"
 #include "ui_azimuth.h"
-#include <QPixmap>
 
 Azimuth::Azimuth(QWidget *parent) :
 	QDialog(parent),
 	ui(new Ui::Azimuth)
 {
 	ui->setupUi(this);
-
-	QPixmap image("/usr/local/Data/Images/azimuth.png") ;
-
-	ui -> picture -> setPixmap(image) ;
-	ui -> picture -> setScaledContents(true) ;
 
 }
 
--- GUI/elevation.cpp
+++ GUI/elevation.cpp
@@ -1,17 +1,11 @@
 #include "elevation.h"
 #include "ui_elevation.h"
-#include <QPixmap>
 
 Elevation::Elevation(QWidget *parent) :
 	QDialog(parent),
 	ui(new Ui::Elevation)
 {
 	ui->setupUi(this);
-
-	QPixmap image("/usr/local/Data/Images/elevation.png") ;
-
-	ui -> picture -> setPixmap(image) ;
-	ui -> picture -> setScaledContents(true) ;
 
 }
 
