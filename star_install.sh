7101b95500777a1fd5eceaa4d238c40721b3eaee
IamNotProgrammer <49314448+IamNotProgrammer@users.noreply.github.com>
2020-06-27 00:31:07 +0200

    Update star_install.sh

--- star_install.sh
+++ star_install.sh
@@ -7,10 +7,13 @@ make -C ./build/
 sudo chmod +x ./build/star-height
 sudo chmod -R 666 ./Data/
 sudo chmod 755 ./Data/
-sudo chmod 777 ./Data/Observatories/
+sudo chmod 755 ./Data/Observatories/
 sudo chmod 755 ./Data/Images/
 sudo chmod 755 ./Data/Plot.gnu
 
+sudo chown root:root ./build/star-height
+sudo chown $USER:$USER ./Data/Observatories/
+sudo chown $USER:$USER ./Data/Observatories/*
 
 sudo mv ./build/star-height /usr/local/bin/
 sudo cp -r ./Data/ /usr/local/
