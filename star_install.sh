c2fd120b915ea3e97b225254ad91321a22248895
IamNotProgrammer <49314448+IamNotProgrammer@users.noreply.github.com>
2020-07-02 19:19:11 +0200

    Add files via upload

--- star_install.sh
+++ star_install.sh
@@ -4,16 +4,11 @@ mkdir build
 qmake -o ./build/ ./GUI/GUI.pro
 make -C ./build/
 
-sudo chmod +x ./build/star-height
-sudo chmod -R 666 ./Data/
-sudo chmod 755 ./Data/
-sudo chmod 755 ./Data/Observatories/
-sudo chmod 755 ./Data/Images/
-sudo chmod 755 ./Data/Plot.gnu
+sudo chmod 755 ./build/star-height
+sudo chmod -R 755 ./Data/
 
 sudo chown root:root ./build/star-height
-sudo chown $USER:$USER ./Data/Observatories/
-sudo chown $USER:$USER ./Data/Observatories/*
 
 sudo mv ./build/star-height /usr/local/bin/
 sudo cp -r ./Data/ /usr/local/
+
